b061e1170a8881bec430903756bc58ebf97fb05c
slix187 <dennisbaumgaertner187@gmail.com>
2024-11-15 13:34:48 +0100

    Added some files and continued with exercises

--- CLearning/findUniqueNumbers.c
+++ CLearning/findUniqueNumbers.c
@@ -1,39 +1,128 @@
+/***************************************************************************************************
+  (c) NewTec GmbH System-Entwicklung und Beratung 2015   -   www.newtec.de
+  $URL: https://svn.newtec.zz/CZSMT/CAN-Rack-19Zoll/trunk/DAC-Karte/DAC-Karte-SW/system/Coding/App/Src/CTemplate.c $
+***************************************************************************************************/
+/**
+@addtogroup gggggModule_group gggggModule
+@{
+@file       xxxxx.c
+
+Enter short description here.
+
+For a detailed description see the detailed description in @ref xxxxx.h
+
+@version    %$Id: CTemplate.c 350 2022-04-14 08:48:14Z heitzmann $
+* @}
+***************************************************************************************************/
+
+/* INCLUDES ***************************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+/* CONSTANTS **************************************************************************************/
+#define MAX_INPUT_NUMBER 8 /**< The size of the input data */
+/* MACROS *****************************************************************************************/
 
-#define SIZE 5
+/* TYPES ******************************************************************************************/
 
-void findUnique(int array[SIZE])
+/* PROTOTYPES *************************************************************************************/
+static void findNonUnique(int numbersToCheck[MAX_INPUT_NUMBER]); /**<IN Function to find duplicate Numbers in test data. */
+static void findUnique(int numbersToCheck[MAX_INPUT_NUMBER]); /**<IN Function to find unique numbers in test data */
+/* VARIABLES **************************************************************************************/
+
+/* EXTERNAL FUNCTIONS *****************************************************************************/
+extern void main(void)
 {
-    int counter;
-    int temp;
-    printf("Unique numbers: ");
-    for(int i = 0; i < SIZE-1; i++)
+    /*Filling list with test data*/
+    int numbersToCheck[MAX_INPUT_NUMBER] = {5, 7, 1, 5, 2, 5, 6, 8}; 
+
+    /* Call function to find uniqe numbers*/
+    findUnique(numbersToCheck);
+
+    /* Call function to find non uniqe numbers*/
+    findNonUnique(numbersToCheck);
+}
+/* INTERNAL FUNCTIONS *****************************************************************************/
+
+static void findNonUnique(int numbersToCheck[MAX_INPUT_NUMBER])
+{
+    int countDuplicateNumbers = 0;
+    bool foundNotUnique = false;
+
+    /* Compare List items*/
+    for(int idx = 0; idx < MAX_INPUT_NUMBER; idx++)
     {
-        int uniqueFound = 0;
-            if(array[i] != array[i+1])
+        /* Reset Flag befor compare */
+        foundNotUnique = false; 
+        /* Inner loop to compare each element in array */
+        for (int jdx = 0; jdx < MAX_INPUT_NUMBER; jdx++)
+        {   
+            /* avoid checking self */
+            if(idx == jdx)
             {
-                counter++;
-                printf("%d", array[i+1]);
+                continue;
             }
+            /* found duplicate set flag to true and exit the inner loop*/
+            if(numbersToCheck[idx] == numbersToCheck[jdx])
+            {
+                foundNotUnique =  true;
+                break;
+            }
+        }
+        /*  */
+        if(foundNotUnique == true)
+        {
+            printf("Not unique: %d\n", numbersToCheck[idx]);
+            countDuplicateNumbers++;
+        }
+        else
+        {
+            foundNotUnique = false;
+        }
 
     }
 
-    printf("\ncounter: %d", counter);
-    
+    printf("\ncounter: %d\n", countDuplicateNumbers);    
 }
 
-
-int main(int argc, char const *argv[])
+/* Function to find unique numbers from test data */
+void findUnique(int numbersToCheck[MAX_INPUT_NUMBER])
 {
-    /*Initializing array and declare it*/
-    int array[SIZE] = {5, 7, 1, 5, 2}; 
+    int countUniqueNumbers = 0;
+    bool foundDuplicate = false;
 
+    /* iterate over array to check if numbers are not equal*/
+    for(int idx = 0; idx < MAX_INPUT_NUMBER; idx++)
+    {
+        /* inner loop to compare each element in array*/
+        for (int jdx = 0; jdx < MAX_INPUT_NUMBER; jdx++)
+        {
+            /* avoid checking self */
+            if(idx == jdx)
+            {
+                continue;
+            }
+            /* if the 2 elements are equal we know its not unique and set flag to true */
+            if(numbersToCheck[idx] == numbersToCheck[jdx])
+            {
+                foundDuplicate =  true;
+                break;
+            }
+        }
 
-
-
-    findUnique(array);
+        /* check for flag and print the non unique values and count them */
+        if(foundDuplicate == false)
+        {
+            printf("Unique values: %d\n", numbersToCheck[idx]);
+            countUniqueNumbers++;
+        }
+        else
+        {
+            foundDuplicate = false;
+        }
+        
+    }
+    /* print the counter for unique numbers */
+    printf("\ncounter: %d\n", countUniqueNumbers);
     
-
-
 }
---
+++ CTemplate.h
@@ -0,0 +1,46 @@
+/***************************************************************************************************
+  (c) NewTec GmbH System-Entwicklung und Beratung 2015   -   www.newtec.de
+  $URL: https://svn.newtec.zz/CZSMT/CAN-Rack-19Zoll/trunk/DAC-Karte/DAC-Karte-SW/system/Coding/App/Inc/CTemplate.h $
+***************************************************************************************************/
+/**
+@addtogroup gggggModule_group gggggModule
+@{
+@file       xxxxx.h
+
+Enter short description here.
+
+Enter detailed description here.
+
+@version    %$Id: CTemplate.h 350 2022-04-14 08:48:14Z heitzmann $
+* @}
+***************************************************************************************************/
+#ifndef XXXXX_H
+#define XXXXX_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* INCLUDES ***************************************************************************************/
+#include "Types.h"
+
+/* CONSTANTS **************************************************************************************/
+
+/* MACROS *****************************************************************************************/
+
+/* TYPES ******************************************************************************************/
+/** Statuses of the module */
+typedef enum
+{
+    XXXXX_RET_OK = 0,   /**< Ok */
+    XXXXX_RET_ERROR     /**< Not otherwise specified error */
+} xxxxx_Ret;
+
+/* PROTOTYPES *************************************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* XXXXX_H */
