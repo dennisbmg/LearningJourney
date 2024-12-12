9b482f5b1e433577ca0046d2eb541616e08d26f3
slix187 <dennisbaumgaertner187@gmail.com>
2024-12-12 09:47:53 +0100

    Starting Python

---
+++ PythonLearning/kontrollstrukturen.py
@@ -0,0 +1,36 @@
+number = 13
+if number < 42:
+    print("ich bin kleiner als 42: " + str(number))
+else:
+    print("ich bin nicht kleiner als 42: " + str(number))    
+
+print("nichtmehr eingerückt")
+
+number = 50
+if number < 42:
+    print("ich bin kleiner als 42: " + str(number))
+else:   
+    print("ich bin nicht kleiner als 42: " + str(number))    
+    
+
+# and == &&
+# or == ||
+# & == & bitwise
+# | == | bitwise
+counter = 0
+
+while counter != 3:
+    print("hello world")
+    
+    counter += 1
+    
+for i in range(10):
+    print(i)
+
+liste = [ 3, 9, 10]
+for i in range (0, 3):
+    print(liste[i])
+
+for i in liste:
+    print(i)
+    
